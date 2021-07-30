ab57a06712dbbffa4764755df14ad78f20fdc422
Luis Gustavo Macedo <luisgustavomacedo13@gmail.com>
2021-07-30 00:52:00 -0300

    Trocando de class para function

--- todoappmain/src/services/getUser.js
+++ todoappmain/src/services/getUser.js
@@ -1,32 +1,27 @@
 import React from "react"
 
-export default class GetUser extends React.Component{
-    constructor(props, TOKEN){
-        super(props)
-        this.token = TOKEN;
-        this.state = {
-            datasUsers: ''
-        }
-    }
-
-    getUsers(){
-        fetch('https://graphql.datocms.com/',
+export default function getUsers(){
+    const token = process.env.REACT_APP_TOKEN
+    let datasUsers = []
+    fetch('https://graphql.datocms.com/',
         {
             method: 'POST',
             headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
-            'Authorization': `Bearer ${this.token}`
+            'Authorization': `Bearer ${token}`
             },
             body: JSON.stringify({
             query: '{ allUsers { usuario, password, createSlug } }'
             })
-        }).then(res => res.json())
-            .then(datas => {
-                this.setState({
-                    datasUsers: datas.data.allUsers
-                }, () => { console.log(this.state.datasUsers) })
-            })
-            .catch(err => console.log(err))
-    }
+        }).then(res => {
+            console.log(res.json())
+            return res.json()
+        })
+        .then(datas => {
+            datasUsers = datas.data.allUsers
+            console.log(datasUsers)
+        })
+        .catch(err => console.log(err))
+        return datasUsers
 }
